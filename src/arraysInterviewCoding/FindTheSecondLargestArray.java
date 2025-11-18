d1c74e46d8a83b6a26c9494be11ef3ee0382df7f
mohammed <shahidmasood666@yahoo.com>
2025-11-17 19:02:43 -0500

    LogicBuildingExcercise-SecondLargestElementInArray

--- src/arraysInterviewCoding/FindTheSecondLargestArray.java
+++ src/arraysInterviewCoding/FindTheSecondLargestArray.java
@@ -4,7 +4,26 @@ public class FindTheSecondLargestArray {
 
 	public static void main(String[] args) {
 		
+		int largestNumber =Integer.MIN_VALUE;
+		int secondLargest =Integer.MIN_VALUE;
+		
+		int arr [] = { 1,3,2,7,5} ;
+		
+		for(  int no : arr ) {
+			if(no>largestNumber) {
+				secondLargest=largestNumber;
+				largestNumber=no;
+			} else if (no >secondLargest && no!=largestNumber) {
+				secondLargest=no;
+			}
+			
+		}
+		
+		System.out.println("Largest Number is " + largestNumber);
+		System.out.println("Second largest Number is  " + secondLargest);
 
+		
+		
 	}
 
 }
